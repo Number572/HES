c424ba387a200958d2728de69cd2f8fb7d6339db
Number571 <email@email.com>
2021-05-18 20:13:22 +0700

    eee

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/number572/hes
 
 go 1.16
+
+require github.com/number571/gopeer v1.2.9 // indirect
--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"io"
 	"net/http"
 	"os"
+	gp "github.com/number571/gopeer"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
+	gp.GenerateKey(1024)
 	io.WriteString(w, "hello, world!")
 }
 
